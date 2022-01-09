465c1e51ddd7223d07070bd1eb672a6f72d5e3b9
Gowtham <gowtham.mandla1@gmail.com>
2022-01-09 17:31:42 +0530

    added results page

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
-const express=require('express');
-const cors = require('cors');
+import express from 'express';
+import mongoose from 'mongoose';
+// import cors form 'cors';
+
+const { MongoClient } = require('mongodb');
+
+const mongodb_url = "mongodb+srv://<username>:<password>@cipher-results.5uw4z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 
 
 const signup = require("./routes/signup")
 const login = require('./routes/login')
 
-
-
 const app = express();
 
 app.use(express.json())
 app.use(cors())
+
 
 app.get('/',(req,res)=>{res.json('Welcome')})
 
