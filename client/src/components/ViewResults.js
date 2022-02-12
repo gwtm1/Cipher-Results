6382a1b7fa9ee0de30432559390a2cd1cf6d1a2f
Jithendra71 <jithendra4567890@gmail.com>
2022-02-12 23:24:40 +0530

    implemented upload results

--- client/src/components/ViewResults.js
+++ client/src/components/ViewResults.js
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import css from "../css/ViewResults.module.css";
+const NodeRSA = require('node-rsa')
 
 const ViewResults = (props) => {
   // const navigate = useNavigate();
 
   const { userId } = props;
   const [semester, setSemester] = useState("");
-  const [privateKeyFile, setPrivateKeyFile] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
 
   const resultsViewHandler = (event) => {
 
     event.preventDefault();
 
     const body = new FormData();
-    body.append(privateKeyFile);
+    body.append(privateKey);
     body.append({ semester, userId });
 
     fetch("http://localhost:8080/viewresults", {
@@ -24,7 +25,7 @@ const ViewResults = (props) => {
         "Content-Type": "application/json",
         jwtkey: localStorage.getItem("jwtToken"),
       },
-      body: body,
+      body: { semester,userId },
     })
       .then((res) => res.json())
       .then((data) => {
@@ -32,11 +33,28 @@ const ViewResults = (props) => {
           alert(data.error);
         } else {
           // complete this
+          decryptResults(data)
         }
       })
       .catch((err) => {
         console.log(err);
       });
+  };
+
+  const decryptResults = data =>{
+    const private_key = new NodeRSA(privateKey);
+      const decryptedResult = private_key.decrypt(data.result, "utf8");
+      console.log(decryptedResult);
+  }
+
+  const onPrivateKeyChange = (event)=>{
+    var fileReader = new FileReader();
+    fileReader.onload = onFileLoad;
+    fileReader.readAsText(event.target.files[0]);
+  }
+
+  const onFileLoad = (event) => {
+    setPrivateKey(event.target.result);
   };
 
   return (
@@ -55,15 +73,12 @@ const ViewResults = (props) => {
           />
         </Form.Group>
 
-        <Form.Group
-          controlId="formFile"
-          onChange={(event) => {
-            setPrivateKeyFile(event.target.value);
-          }}
-          className="mb-3"
-        >
+        <Form.Group controlId="formFile" className="mb-3">
           <Form.Label>Upload Private Key</Form.Label>
-          <Form.Control type="file" />
+          <Form.Control 
+            type="file"
+            onChange={(event)=>{onPrivateKeyChange(event)}}   
+          />
         </Form.Group>
         <Button
           variant="primary"
--- server/controllers/viewresults.js
+++ server/controllers/viewresults.js
@@ -1,3 +1,4 @@
+import Students from "../models/student.js";
 import { sendError } from "../utils/helper.js";
 // import csvtojson from "convert-csv-to-json";
 
@@ -5,10 +6,11 @@ export const viewresults = (req, res) => {
     try {
         const { semester, userId } = req.body;
         // const { privateKeyFile, semester, userId } = req.body;
+        const currStudent = await Students.findById(userId, "results");
 
-        console.log("Hi", semester, userId);
+        // console.log("Hi", semester, userId);
         // console.log( privateKeyFile, semester, userId);
-        res.send("Inside viewresults controller");
+        res.send({ result:currStudent.results[semester-1] });
         
     } catch(error) {
         console.log(error.message);
