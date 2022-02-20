763c297cda649c87134761289e01002be95637a3
Gowtham <gowtham.mandla1@gmail.com>
2022-02-20 20:33:23 +0530

    Completed CSS

--- client/src/components/DisplayResults.js
+++ client/src/components/DisplayResults.js
@@ -1,35 +1,31 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Table } from "react-bootstrap";
+import css from '../css/DisplayResults.module.css'
 
 const DisplayResults = (props) => {
+  
   const { results, isloggedIn } = props;
-  // const results = { sub1: 100, sub2: 200, sub3: 400, sub4: 60, sub5: 90 };
+  // const results = { "rollnumber":"2019IMT-058", sub1: 100, sub2: 200, sub3: 400, sub4: 60, sub5: 90 };
   // const { sub1, sub2, sub3 } = results;
 
   const grades = JSON.parse(results);
-  console.log(props, "hello", grades);
+  console.log("hello", grades);
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!isloggedIn) {
-      navigate("/login", { replace: true });
-    }
-  }, [isloggedIn, navigate]);
+    useEffect(() => {
+      if (!isloggedIn) {
+        navigate("/login", { replace: true });
+      }
+    }, [isloggedIn, navigate]);
 
   return (
-    <Table bordered hover variant="dark">
-      {/* <thead>
-        <tr>
-          <th> {} </th>
-        </tr>
-      </thead>
-      <thead>
-        <tr>
-          <th> Subject </th>
-        </tr>
-      </thead> */}
+  <div className={css.container}>
+    <div className={css.heading}>Grade Report</div>
+    <div className={css.rollnumber}>{grades.rollnumber}</div>
+
+    <table bordered hover variant="dark">
       <thead>
         <tr>
           <th> Subject </th>
@@ -47,7 +43,8 @@ const DisplayResults = (props) => {
           } else return ''
         })}
       </tbody>
-    </Table>
+    </table>
+    </div>
   );
 };
 
